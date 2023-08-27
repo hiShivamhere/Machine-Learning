efbd221f60f069d5289ec1528274e7d3c9156826
Shivam Gupta <49103969+hiShivamhere@users.noreply.github.com>
2023-08-27 16:55:51 +0530

    3rd problem added

---
+++ 04_Pandas_Problems/03_RearrangeProductsTable.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    df = products.melt(id_vars=['product_id'], value_vars=['store1', 'store2', 'store3'], var_name='store', value_name='price')
+    df.dropna(subset=['price'],inplace=True)
+    return df
